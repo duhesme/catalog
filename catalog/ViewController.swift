14ed1cb8d974351348439276fda50d6da66bcc77
duhesme <hitechwatchout@gmail.com>
2022-08-25 14:42:12 +0300

    Initial commit.

---
+++ catalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catalog
+//
+//  Created by Никита Владимирович on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
